184324b2520b653a00b37adf660b65bb3f698a3b
Tejas Nikhar <tejastn10@gmail.com>
2024-12-02 17:07:27 +0530

    feat: update checkURL to parse URL

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,14 +1,22 @@
 package monitor
 
 import (
+	"errors"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 // CheckURL checks the URL and returns the HTTP status code, response time, and any error.
-func CheckURL(url string) (int, time.Duration, error) {
+func CheckURL(urlString string) (int, time.Duration, error) {
+	// Validate the URL to ensure it's well-formed and from a trusted source
+	parsedURL, err := url.Parse(urlString)
+	if err != nil || !parsedURL.IsAbs() {
+		return 0, 0, errors.New("invalid URL or not an absolute URL")
+	}
+
 	start := time.Now()
-	response, err := http.Get(url)
+	response, err := http.Get(urlString)
 	elapsed := time.Since(start)
 
 	if err != nil {
