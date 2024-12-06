39f900a303089c9b41aac640b2a3e27bf046b9cd
Tejas Nikhar <tejastn10@gmail.com>
2024-12-06 10:31:30 +0530

    feat: add 2 more flags to take as optional input

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	logTimestamp := flag.Bool("logTimeStamp", true, "Set to true to log timestamps. Timestamps are logged by default in the file")
 	url := flag.String("url", "https://example.com", "The URL to monitor. Default is https://example.com")
 	interval := flag.Int("interval", 30, "The monitoring interval in seconds. Default is 30 seconds")
+	retryCount := flag.Int("retryCount", 3, "The number of retries for monitoring requests. Default is 3 retries")
+	backoffDuration := flag.Int("backoffDuration", 2, "The backoff duration (in seconds) between retries. Default is 2 seconds")
 
 	// Parse the flags
 	flag.Parse()
